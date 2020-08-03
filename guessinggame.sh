0b3a0fd9994b602cfc8aeb2d920b3d3befa3ea2b
kkwan99 <47930111+kkwan99@users.noreply.github.com>
2020-08-03 13:39:26 -0400

    changed guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,25 @@
-function guessinggame {
-  echo "Guess how many files are in your current directory"
- # read response
- # echo "You entered $response"
+guessinggame () {
+     echo "How many files are in the current directory?"
+     echo "Please type in a guess and then press Enter"
+     read response
+  while true
+  do
+     nb_file=$(ls | wc -l)
+     if [[ $response -lt $nb_file ]]
+     then
+       echo "Your guess was too low"
+       echo "Please guess again and then press Enter"
+       read response
+     elif [[ $response -gt $nb_file ]]
+     then
+       echo "Your guess was too high"
+       echo "Please guess again and then press Enter"
+       read response
+     else
+       echo "Congratulations, you guessed correctly"
+       echo "There are $response files in the current directory"
+       break
+     fi
+  done
 }
+guessinggame
